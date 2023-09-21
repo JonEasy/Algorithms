c36f65e00a7f1d9589ad2c5a0c6f633806c916eb
Jonel <jonel.palouvisa@ge.com>
2023-09-21 12:14:26 +0200

    Insertion 1 and 3 with inner classes

--- src/main/java/Algorithms/sort/Insertion1/Solution.java
+++ src/main/java/Algorithms/sort/Insertion1/Solution.java
@@ -33,15 +33,44 @@ class Result {
             arr.set(0, elementSort);
         }
 
-        for (int index=0; index<n; index++) {
-            System.out.print(arr.get(index) + " ");
-        }
-        System.out.println();
-    }
-
-}
-
 public class Solution {
+    static class Result {
+
+      /*
+       * Complete the 'insertionSort1' function below.
+       *
+       * The function accepts following parameters:
+       *  1. INTEGER n
+       *  2. INTEGER_ARRAY arr
+       */
+
+        private static void insertionSort1(int n, List<Integer> arr) {
+
+          int elementSort = arr.get(n-1);
+          //System.out.println("Element sort is " + elementSort);
+          for (int i=n-2; i>=0; i--) {
+              //System.out.println("Current index is " + i + " with element " + arr.get(i) + " and element sort is " + elementSort);
+              if (arr.get(i)<=elementSort) {
+                  arr.set(i+1,  elementSort);
+                  break;
+              }
+              arr.set(i+1, arr.get(i));
+              for (int index=0; index<n; index++) {
+                  System.out.print(arr.get(index) + " ");
+              }
+              System.out.println();
+          }
+          if (arr.get(0)>elementSort) {
+              arr.set(0, elementSort);
+          }
+
+          for (int index=0; index<n; index++) {
+              System.out.print(arr.get(index) + " ");
+          }
+          System.out.println();
+      }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -62,3 +91,14 @@ public class Solution {
         bufferedReader.close();
     }
 }
+
+// Sample Input
+
+// 5
+// 2 4 6 8 3
+// Sample Output
+
+// 2 4 6 8 8 
+// 2 4 6 6 8 
+// 2 4 4 6 8 
+// 2 3 4 6 8
--- src/main/java/Algorithms/sort/Insertion2/Solution.java
+++ src/main/java/Algorithms/sort/Insertion2/Solution.java
@@ -1,56 +1,53 @@
 package Algorithms.sort.Insertion2;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-class Result {
-
-    /*
-     * Complete the 'insertionSort2' function below.
-     *
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER_ARRAY arr
-     */
-    public static void printArray(List<Integer> arr){
-        for (Integer element: arr) {
-            System.out.print(element+ " ");
-        }
-        System.out.println();
-    }
-
-    public static void insertionSort2(int n, List<Integer> arr) {
-
-        for (int index=1; index <n; index ++) {
-            int elementSort = arr.get(index);
-
-            for (int backIndex=index-1; backIndex>=0; backIndex--){
-
-                if (arr.get(backIndex)<=elementSort) {
-
-                    arr.set(backIndex+1, elementSort);
-                    printArray(arr);
-                    break;
-                }
-                arr.set(backIndex+1, arr.get(backIndex));
-            }
-            if (arr.get(0)>elementSort) {
-                arr.set(0, elementSort);
-                printArray(arr);
-            }
-        }
-
-
-    }
-
-}
 
 public class Solution {
+    static class Result {
+
+        /*
+         * Complete the 'insertionSort2' function below.
+         *
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER_ARRAY arr
+         */
+        public static void printArray(List<Integer> arr){
+            for (Integer element: arr) {
+                System.out.print(element+ " ");
+            }
+            System.out.println();
+        }
+
+        public static void insertionSort2(int n, List<Integer> arr) {
+
+            for (int index=1; index <n; index ++) {
+                int elementSort = arr.get(index);
+
+                for (int backIndex=index-1; backIndex>=0; backIndex--){
+
+                    if (arr.get(backIndex)<=elementSort) {
+
+                        arr.set(backIndex+1, elementSort);
+                        printArray(arr);
+                        break;
+                    }
+                    arr.set(backIndex+1, arr.get(backIndex));
+                }
+                if (arr.get(0)>elementSort) {
+                    arr.set(0, elementSort);
+                    printArray(arr);
+                }
+            }
+
+
+        }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
